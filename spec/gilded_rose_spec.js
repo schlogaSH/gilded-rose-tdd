a7c8f90c968842a243807e248051565c59ddd376
schlogaSH <stephanschlager@gmail.com>
2025-07-01 19:50:46 +0200

    Test: Normaler Artikel verliert 1 Qualität und 1 Verkaufstag pro Tag

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,9 +1,10 @@
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    items = [ new Item("foo", 0, 0) ];
+  it("reduziert Qualität und Verkaufstage eines normalen Items um 1", function() {
+    items = [ new Item("Normal Item", 10, 20) ];
     update_quality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(19);
   });
 
 });
