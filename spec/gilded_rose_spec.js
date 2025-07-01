99b5db9e95185457cb77943ecd076a07ef944a7a
schlogaSH <stephanschlager@gmail.com>
2025-07-01 20:01:39 +0200

    Test: 'Aged Brie' gewinnt nach Ablaufdatum doppelt an Qualität

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -13,6 +13,12 @@ describe("Gilded Rose", function() {
     expect(items[0].sell_in).toEqual(1);
     expect(items[0].quality).toEqual(1);
   });
+    it("'Aged Brie' gewinnt nach Ablaufdatum doppelt an Qualität", function() {
+    items = [ new Item("Aged Brie", 0, 0) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(-1);
+    expect(items[0].quality).toEqual(2);
+  });
 
 
 });
