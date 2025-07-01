c95b02ed9102a296c8b8f6c28e4b4772d8ee87cd
schlogaSH <stephanschlager@gmail.com>
2025-07-01 20:05:39 +0200

    Test: 'Backstage passes' steigen um +2 bei 10 Tagen oder weniger

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -34,6 +34,13 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(21);
   });
 
+    it("'Backstage passes' steigen um +2 bei 10 Tagen oder weniger", function() {
+    items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 25) ];
+    update_quality();
+    expect(items[0].sell_in).toEqual(9);
+    expect(items[0].quality).toEqual(27);
+  });
+
 
 });
 
